a68ef0dd544e70c18fed105cc75a14355c667f04
nokopi <nokopi36@gmail.com>
2025-11-19 23:30:56 +0900

    feat: バージョン更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nokopi.stopwatchtimer"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
